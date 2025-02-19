fc9fe5e64510eedc1e8b69a9c20996c5ba15aa5a
Ali Usman <ali.usman@tapcheck.com>
2025-02-19 10:43:04 +0500

    When Block

--- app/src/main/java/com/example/fa22_bse_a/basics/control_block/main.kt
+++ app/src/main/java/com/example/fa22_bse_a/basics/control_block/main.kt
@@ -5,28 +5,49 @@ import java.util.Scanner
 fun main() {
     println("Here we are -- Control Block")
 
-    val grade: Char?
-    println("Enter your Grade")
-    grade = Scanner(System.`in`).nextLine().uppercase().getOrNull(0)//.get(0)//[0]  //
+    val age: Int?
+    println("Enter your Age")
 
-    when(grade) {
-        'A' -> {
-            print("Excelent")
+    age = Scanner(System.`in`).nextLine().toIntOrNull()//.get(0)//[0]  //
+
+    when(age) {
+        in (1..20) -> {
+            // Code to execute when age is between 1 and 20
+            println("Enjoy")
         }
-        'B' -> {
-            print("Good")
+        in (21 .. 40) -> {
+            println("Count Down started")
         }
-        'C' -> {
-            print("Perha kro")
-        }
-        'D' -> {
-            print("Next time DI ho jao gy")
+        in (100 .. 10000) -> {
+            println("You are a snake")
         }
         else -> {
-            print("Ja ja tur ja")
+            // Code to execute for all other cases
+            println("Age is outside the 1 to 20 range")
         }
-
     }
+//    val grade: Char?
+//    println("Enter your Grade")
+//    grade = Scanner(System.`in`).nextLine().uppercase().getOrNull(0)//.get(0)//[0]  //
+//
+//    when(grade) {
+//        'A' -> {
+//            print("Excelent")
+//        }
+//        'B' -> {
+//            print("Good")
+//        }
+//        'C' -> {
+//            print("Perha kro")
+//        }
+//        'D' -> {
+//            print("Next time DI ho jao gy")
+//        }
+//        else -> {
+//            print("Ja ja tur ja")
+//        }
+//
+//    }
 
 
 }
