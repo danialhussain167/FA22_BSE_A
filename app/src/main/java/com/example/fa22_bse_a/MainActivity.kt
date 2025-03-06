9eb83d7ac2881e30f16d455b766793337d3e40e8
Ali Usman <ali.usman@tapcheck.com>
2025-03-06 10:43:40 +0500

    CardView impl

--- app/src/main/java/com/example/fa22_bse_a/MainActivity.kt
+++ app/src/main/java/com/example/fa22_bse_a/MainActivity.kt
@@ -8,13 +8,30 @@ import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.student_card_activity)
+
+        val studentCardRef: CardView = findViewById(R.id.student_card)
+
+        val rotationJob = GlobalScope.launch {
+            while(true) {
+                for (angle in 0..360) {
+                    studentCardRef.rotation = angle.toFloat()
+                    delay(30)
+                }
+            }
+        }
+
+
     }
 }
