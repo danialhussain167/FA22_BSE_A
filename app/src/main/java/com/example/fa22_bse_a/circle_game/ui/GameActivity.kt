dbbdf5251bdf93f78d44678c23d813ea0d788c6b
Ali Usman <ali.usman@tapcheck.com>
2025-04-28 12:33:11 +0500

    Corotines lecture lab with game topic

--- app/src/main/java/com/example/fa22_bse_a/circle_game/ui/GameActivity.kt
+++ app/src/main/java/com/example/fa22_bse_a/circle_game/ui/GameActivity.kt
@@ -2,27 +2,23 @@ package com.example.fa22_bse_a.circle_game.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+
 
 class GameActivity : AppCompatActivity() {
 
     var score = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val gameView = GameScreen(this@GameActivity, 50f) {
-            score++
-            println("Current Score = $score")
-        }
+        val gameView = GameScreen(this, 50f, onCircleTouchCallBack =  {
+            println("Current Score  = $it")
+        })
         setContentView(gameView)
 
-        lifecycleScope.launch {
-//            while (true) {
-                delay(500)
-                gameView.drawAtRandomPosition()
+//        lifecycleScope.launch {
+//            while(true) {
+//                delay(200)
+//                gameView.drawCircleAtRandomPosition()
 //            }
-
-        }
+//        }
     }
 }
